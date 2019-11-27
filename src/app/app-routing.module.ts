1341ea4b3f46f4f82c057389d907178e6bb967b9
Zach Dean <zdean3721@gmail.com>
2019-11-26 19:52:27 -0600

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { LoginComponent } from './login/login.component';
 import { BusinessCardsComponent } from './business-cards/business-cards.component';
 import { AuthGuard } from './auth/auth.guard';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { NewbusinesscardComponent } from './newbusinesscard/newbusinesscard.component';
+import { UpdateComponent } from './update/update.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'businessCards', component: BusinessCardsComponent, canActivate: [AuthGuard] },
+  { path: 'newCard', component: NewbusinesscardComponent, canActivate: [AuthGuard] },
+  { path: 'update', component: UpdateComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent }
 ];
 
